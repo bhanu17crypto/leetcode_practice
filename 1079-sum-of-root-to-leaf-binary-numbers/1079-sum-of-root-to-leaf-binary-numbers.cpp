e0f45c97671a616e59d8949966c1e6047714b8cb
Bhanu Pratap <80469274+bhanu17crypto@users.noreply.github.com>
2023-10-18 12:25:24 +0530

    Time: 7 ms (37.14%), Space: 17.1 MB (45.14%) - LeetHub

--- 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.cpp
@@ -9,22 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+//     int ans=0;
+// public:
+//     int sumRootToLeaf(TreeNode* root) {
+//         int buff = 0;
+//         dfs(root, buff);
+//         return ans;    
+//     }
+    
+//     void dfs(TreeNode *root, int buff){
+//         buff = buff<<1 + root->val;
+//         if(!root->left && !root->right){
+//             ans += buff;
+//             return;
+//         }
+//         if(root->left) dfs(root->left, buff);
+//         if(root->right) dfs(root->right, buff);
+//     }
+// };
 class Solution {
-    int ans=0;
 public:
-    int sumRootToLeaf(TreeNode* root) {
-        int buff = 0;
-        dfs(root, buff);
-        return ans;    
+    int sumRootToLeafHelper(TreeNode* rt, int cur_val) {
+        if(! rt) return 0;
+        
+        cur_val = (cur_val << 1) | rt->val;
+        
+        if(!rt->left && !rt->right)
+			return cur_val;
+			
+        return sumRootToLeafHelper(rt->left, cur_val) + sumRootToLeafHelper(rt->right, cur_val);
     }
     
-    void dfs(TreeNode *root, int buff){
-        buff = buff*2 + root->val;
-        if(!root->left && !root->right){
-            ans += buff;
-            return;
-        }
-        if(root->left) dfs(root->left, buff);
-        if(root->right) dfs(root->right, buff);
+    int sumRootToLeaf(TreeNode* rt) {
+        return sumRootToLeafHelper(rt, 0);
     }
 };
